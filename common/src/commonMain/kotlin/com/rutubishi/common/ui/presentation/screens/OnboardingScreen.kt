807a9238b93cb087b26da2b77fc85c4108036e4c
otsembo <okumu.otsembo@gmail.com>
2023-05-16 14:07:35 +0300

    App: Tmp

--- common/src/commonMain/kotlin/com/rutubishi/common/ui/presentation/screens/OnboardingScreen.kt
+++ common/src/commonMain/kotlin/com/rutubishi/common/ui/presentation/screens/OnboardingScreen.kt
@@ -1,5 +1,6 @@
 package com.rutubishi.common.ui.presentation.screens
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
