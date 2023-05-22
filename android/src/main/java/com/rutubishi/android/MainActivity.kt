ba8b24f3428d5de8af8355360c3e6d622f5ed891
otsembo <okumu.otsembo@gmail.com>
2023-05-22 09:00:44 +0300

    App: Add AppBar definition

--- android/src/main/java/com/rutubishi/android/MainActivity.kt
+++ android/src/main/java/com/rutubishi/android/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.rutubishi.android.ui.presentation.components.HomeAppBar
 import com.rutubishi.common.App
+import com.rutubishi.common.ui.theme.TableTopTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+            TableTopTheme {
+                HomeAppBar()
+            }
         }
     }
+
 }
 
 @Composable
