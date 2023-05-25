6eb7ad00cd6c7b3d53e7dbf9c84f71a7a631282e
otsembo <okumu.otsembo@gmail.com>
2023-05-25 17:39:37 +0300

    Server: Add tmp

--- server/src/main/kotlin/com/rutubishi/plugins/GraphQLServer.kt
+++ server/src/main/kotlin/com/rutubishi/plugins/GraphQLServer.kt
@@ -1,36 +1,42 @@
 package com.rutubishi.plugins
 
 import com.apurebase.kgraphql.GraphQL
+import com.rutubishi.data.db.TableTopDBFactory
 import com.rutubishi.data.db.User
 import io.ktor.server.application.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureGraphQL() {
+    TableTopDBFactory.init()
     install(GraphQL){
         endpoint = "/graphql"
         playground = true
         schema {
             query("hello") {
                 resolver { ->
-                    transaction {
-                        repeat(5){
-                            User.new {
-                                this.active = true
-                                this.email = "asdas"
-                                this.passwordHash = "adasdasd"
-                                this.phone = "sdasdas"
-                                this.name = "Adasdasdas"
-                            }
-                        }
-                        commit()
-                    }
-                    val users = transaction {
-                        User.all().toList()
-                    }
-
-                    return@resolver users
+                    sendSampleData()
                 }
             }
         }
     }
+}
+
+fun sendSampleData(): String{
+    transaction {
+        repeat(5){
+            User.new {
+                this.active = true
+                this.email = "asdas"
+                this.passwordHash = "adasdasd"
+                this.phone = "sdasdas"
+                this.name = "Adasdasdas"
+            }
+        }
+        commit()
+    }
+    val users = transaction {
+        User.all().toList()
+    }
+
+    return users.joinToString(",")
 }
