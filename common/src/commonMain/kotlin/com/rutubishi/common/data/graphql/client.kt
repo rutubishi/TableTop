a668a6833ab2491a34ed097e64ca3440270f25ce
otsembo <okumu.otsembo@gmail.com>
2023-06-02 15:21:49 +0300

    GraphQL: Add OkHttp config for GraphQLClient

--- common/src/commonMain/kotlin/com/rutubishi/common/data/graphql/client.kt
+++ common/src/commonMain/kotlin/com/rutubishi/common/data/graphql/client.kt
@@ -1,10 +1,26 @@
 package com.rutubishi.common.data.graphql
 
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.okHttpClient
+import okhttp3.OkHttpClient
 
 object GraphQLClient {
+    private val okHttp =  OkHttpClient
+        .Builder()
+        .addInterceptor {
+            val original = it.request()
+            val request = original.newBuilder()
+                .method(original.method, original.body)
+                .build()
+            it.proceed(request)
+        }
+        .build()
+
+    private const val SERVER_URL = "http://192.168.0.101:3000/graphql"
+
     val client = ApolloClient
         .Builder()
-        .serverUrl("http://localhost:3000/graphql")
+        .okHttpClient(okHttp)
+        .serverUrl(SERVER_URL)
         .build()
 }
